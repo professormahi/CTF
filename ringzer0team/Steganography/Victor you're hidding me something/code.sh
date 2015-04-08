f60666d5f601c74724eca62b553bb08c16316439
Mahdi Fooladgar <professormahi_f@yahoo.com>
2015-04-08 15:29:28 +0430

    Adding Victor ...! Steganography

---
+++ ringzer0team/Steganography/Victor you're hidding me something/code.sh
@@ -0,0 +1,35 @@
+#! /bin/bash
+echo "Fort heureux l'homme qui trouvera
+La vérité au travère des noires
+Abysses d'une âme tourmenter par la
+Galère d'un amour improbable.
+
+Ces âmes que tu rappelles,
+Mon coeur, ne reviennent pas.
+Pourquoi donc s'obstinent-elles,
+Hélas ! à rester là-bas ?
+
+Dans les sphères éclatantes,
+Dans l'azur et les rayons,
+Sont-elles donc plus contentes
+Qu'avec nous qui les aimions ?
+
+Nous avions sous les tonnelles
+Une maison près Saint-Leu.
+Comme les fleurs étaient belles !
+Comme le ciel était bleu !
+
+Parmi les feuilles tombées,
+Nous courions au bois vermeil ;
+Nous cherchions des scarabées
+Sur les vieux murs au soleil ;
+
+On riait de ce bon rire
+Qu'Éden jadis entendit,
+Ayant toujours à se dire
+Ce qu'on s'était déjà dit ;
+
+Je contais la Mère l'Oie ;
+On était heureux, Dieu sait !
+On poussait des cris de joie
+Pour un oiseau qui passait." | cut -c-1 | tr -d '\n'; echo
